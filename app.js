d30665b2f025717061f9d666399be9ddfed52557
Emin Ulusoy <ulusoyeminn@gmail.com>
2023-12-07 15:10:10 +0300

    ilk commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+const hostname = '127.0.0.1'
+
+app.use(express.static('public'))
+
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+
+//* main.handlebars içindeki body toplam 6 tane süslü parantez içinde olacak.
+app.get('/', (req,res)=>{
+    res.render('site/home');
+})
+
+app.get('/contact', (req,res)=>{
+    res.render('site/contact');
+})
+
+
+
+app.listen(port, hostname, () => {
+    console.log(`Server Calısıyor, http://${hostname}:${port}/`)
+})
